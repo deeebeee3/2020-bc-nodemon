2f2b6e0918d144f6672c7fe942078aaa9913a0ba
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-05-26 12:21:34 +0100

    debouncing add events - don't start up users code too often

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
+const debounce = require('lodash.debounce');
 const chokidar = require('chokidar');
 
+//wrap start function with debounce
+const start = debounce(() => {
+    console.log('STARTING USERS PROGRAM');
+}, 100);
+
 chokidar.watch('.')
-    .on('add', () => {console.log('file added')})
+    .on('add', start)
     .on('change', () => {console.log('file changed')})
     .on('unlink', () => {console.log('file unlinked')});
 
-    
