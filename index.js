5c111fd7b5c3517e35aeb4897eacc9348ec981a2
Deepak Bhari <deepakbhari@macbook-pro-2.lan>
2020-05-26 12:11:03 +0100

    watching files with Chokidar

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 #!/usr/bin/env node
 
-console.log('i was executed!');
+const chokidar = require('chokidar');
+
+chokidar.watch('.')
+    .on('add', () => {console.log('file added')})
+    .on('change', () => {console.log('file changed')})
+    .on('unlink', () => {console.log('file unlinked')});
+
+    
